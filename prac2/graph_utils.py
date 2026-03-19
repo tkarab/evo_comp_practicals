6d73f4de0149067ab8f16206672c20561acf508c
tkarab <tasoskarabetsios@gmail.com>
2026-03-19 18:56:06 +0100

    comments

--- prac2/graph_utils.py
+++ prac2/graph_utils.py
@@ -67,6 +67,8 @@ def get_conflict_count(graph: Graph, coloring: GraphColoring) -> int:
     num_colors = coloring.number_of_colors
     conflicts = 0
 
+    # For each color class in the partition representation, check all pair combinations of vertices for potential neighbors
+    # # of neighbors in color class -> conflicts
     for color_index in range(num_colors):
         color_class = coloring.partition[color_index]
 
@@ -90,7 +92,12 @@ def get_conflict_count(graph: Graph, coloring: GraphColoring) -> int:
 #
 #     return conflicts // 2
 
-
+"""
+Returns:
+    - final coloring
+    - solved (True/False)
+    - improvement (True/False)
+"""
 def vertex_descent_iteration(
     graph: Graph,
     coloring: GraphColoring,
@@ -101,34 +108,43 @@ def vertex_descent_iteration(
     vertices_random_order = random.sample(range(n), n)
     improvement = False
 
+    # Count initial number of conflicts
     current_conflicts = get_conflict_count(graph, coloring)
 
     for vertex in vertices_random_order:
         old_color = coloring.assignment[vertex]
 
+        # color_counts: a list of length k, with the counts of total neighbors from each color that the vertex has.
+        #   e.g. color_counts[4] == 5 -> 5 neighbors with the color code '4'
         neighbors = graph.edges[vertex]
         color_counts = [0] * k
 
+        # Filling in the counts of each color
         for neighbor in neighbors:
             neighbor_color = coloring.assignment[neighbor]
             color_counts[neighbor_color] += 1
 
+        # best recoloring candidates: the color codes with the least counts amongst the color_counts list
         min_conflicts_for_vertex = min(color_counts)
         best_color_candidates = [
             color for color, count in enumerate(color_counts)
             if count == min_conflicts_for_vertex
         ]
 
+        # Tie-break: pick new color randomly
         new_color = random.choice(best_color_candidates)
         coloring.change_color(vertex, new_color)
 
+        # Total number of conflicts is only affected by the difference in conflicts due to recoloring current vertex
         new_conflicts = current_conflicts - (
                 color_counts[old_color] - color_counts[new_color]
         )
 
+        # If the last recoloring causes 0 conflicts -> problem solved
         if new_conflicts == 0:
             return coloring, True, True
 
+        # If the last recoloring causes fewer conflicts than before -> set improvement flag to True
         if new_conflicts < current_conflicts:
             improvement = True
 
@@ -149,6 +165,10 @@ def vertex_descent_full_run(
         print(f"\nStarting full Vertex Descent run (max {L} cycles)")
         print(f"Initial Conflicts: {get_conflict_count(graph, coloring)}")
 
+    # Runs vertex descent algorithm until one of the following happens:
+    #   - No improvement is achieved (conflicts remain unchanged)
+    #   - Problem is solved
+    #   - Completed L descent cycles
     while descent_cycles < L:
         if debug:
             print(f"\n--- Descent cycle {descent_cycles + 1} ---")
